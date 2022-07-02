add91d2096ddda8959f5387891e62921158a6969
Zosoutsav <88942040+zosoutsav@users.noreply.github.com>
2022-07-02 17:36:55 +0545

    Update and rename ponggame.py to utsavgautam.py

---
+++ utsavgautam.py
@@ -0,0 +1,5 @@
+my_name ='utsav' 
+subject_Name = 'computer programming and analysis'; 
+year = '1st' 
+country = 'Canada' 
+print(f'Hi, i am {my_name} i am enrolled in {subject_Name} i am in {year}year i am from {country}'); 
